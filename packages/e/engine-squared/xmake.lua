38381c34375cdce6d12f11c967be766b97eaf63f
Miouzora <alexandre.franquet@epitech.eu>
2024-11-13 16:40:59 +0900

    Update xmake.lua

--- packages/e/engine-squared/xmake.lua
+++ packages/e/engine-squared/xmake.lua
@@ -7,44 +7,13 @@ package("engine-squared")
 
     add_urls("https://github.com/EngineSquared/EngineSquared.git")
 
-    -- local core_engine = "EngineSquaredCore"
-
-    -- local plugins = {
-    --     "PluginCamera",
-    --     "PluginCollision",
-    --     "PluginColors",
-    --     "PluginInput",
-    --     "PluginMath",
-    --     "PluginObject",
-    --     "PluginPhysics",
-    --     "PluginScene",
-    --     "PluginTime",
-    --     "PluginUI",
-    --     "PluginUtils",
-    --     "PluginVkWrapper",
-    --     "PluginWindow"
-    -- }
-
-    -- local components = {}
-
-    -- for _, plugin in ipairs(plugins) do
-    --     add_configs(plugin, {description = "Enable the " .. plugin .. " plugin.", default = false, type = "boolean"})
-    -- end
-
-    -- on_component("PluginCamera", function (package, config)
-    --     local e = package:config("shared") and "" or "-s"
-    --     if package:debug() then
-    --         e = e .. "-d"
-    --     end
-
-    --     component:add("deps", "glm")
-    -- end)
-
     on_install(function (package)
         local configs = {}
         if package:config("shared") then
             configs.kind = "shared"
         end
+
+        os.run("xmake build")
 
         import("package.tools.xmake").install(package, configs)
     end)
